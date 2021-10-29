27eacfdf5236bc3207ba05ebf9d6cac5b23dc67a
TanyaTrue <tatianatrue04@gmail.com>
2021-10-29 17:47:59 +0300

    added js for letters

--- script.js
+++ script.js
@@ -29,9 +29,46 @@ function displayWord() {
   const innerWord = wordElement.innerText.replace(/\n/g, "");
 
   if (innerWord === selectedWord){
-    finalMessage.innerText = "Юху! Ты виграл!";
-    popup.style.display ="flex"
+    finalMessage.innerText = "Юху! Ты выиграл!";
+    popup.style.display ="flex";
   }
 }
 
-displayWord()
+//Update wrong letters
+function updateWrongLettersElement(){
+}
+
+//Show notification
+function showNotification(){
+  notification.classList.add('show');
+
+  setTimeout(() => notification.classList.remove('show'), 2100);
+}
+
+//Keydown letter press
+  window.addEventListener("keydown", e => {
+    if(/[а-я]$/i.test(e.key)){
+      const letter = e.key;
+
+      if(selectedWord.includes(letter)){
+        if (!correctLetters.includes(letter)){
+          correctLetters.push(letter);
+
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        if(!wrongLetters.includes(letter)){
+          wrongLetters.push(letter);
+
+          updateWrongLettersElement();
+        } else {
+          showNotification();
+        }
+      }
+    }
+  });
+
+
+displayWord();
